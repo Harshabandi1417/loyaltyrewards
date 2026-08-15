25b41abf9ddd6b0fe931c2d209047eafe60ab56a
Bandi Harshavardhan <bandiharshavardhan777@gmail.com>
2026-08-15 16:21:51 +0000

    Include UI application in main repository

---
+++ app/com.loyaltyrewards.app/webapp/Component.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "./model/models"
+], function (UIComponent, Device, models) {
+    "use strict";
+
+    return UIComponent.extend("com.loyaltyrewards.app.Component", {
+        metadata: {
+            manifest: "json",
+            interfaces: ["sap.ui.core.IAsyncContentCreation"]
+        },
+
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+
+            this.setModel(models.createDeviceModel(), "device");
+
+            this.getRouter().initialize();
+        },
+
+        getContentDensityClass: function () {
+            if (this.contentDensityClass === undefined) {
+                if (
+                    document.body.classList.contains("sapUiSizeCozy") ||
+                    document.body.classList.contains("sapUiSizeCompact")
+                ) {
+                    this.contentDensityClass = "";
+                } else if (!Device.support.touch) {
+                    this.contentDensityClass = "sapUiSizeCompact";
+                } else {
+                    this.contentDensityClass = "sapUiSizeCozy";
+                }
+            }
+
+            return this.contentDensityClass;
+        }
+    });
+});
